60dd9f6a88af6b53842db7bfee6d7ffbd875f013
Saboni6 <saboni140@gmail.com>
2026-09-06 04:52:17 +0000

    Initial Tajawal font module

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Website Tajawal Arabic Font",
+    "version": "17.0.1.0.1",
+    "category": "Website",
+    "summary": "Tajawal font for Arabic website pages",
+
+    "depends": [
+        "website",
+    ],
+
+    "data": [
+        "views/font.xml",
+    ],
+
+    "assets": {
+        "web.assets_frontend": [
+            "website_tajawal_font/static/src/css/tajawal.css",
+        ],
+    },
+
+    "installable": True,
+    "application": False,
+}
